27b9cf39941c6b2789afcb2975033c1bf4672b65
craig1dias-lgtm <craig1dias@gmail.com>
2025-11-22 21:20:08 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://help-center-app-three.vercel.app' : '',
-  publicRuntimeConfig: {
-    basePath: process.env.NODE_ENV === 'production' ? 'https://help-center-app-three.vercel.app' : '',
-  },
+  // No longer need assetPrefix with subdomain approach
   images: {
-    domains: ['help-center-app-three.vercel.app'],
+    domains: ['support.matchmintfootballcards.com', 'help-center-app-three.vercel.app'],
     unoptimized: true,
   },
 }
